147f6dbdd08ccf4e32fe15fb705c1d196c67c641
selimfuentesc-art <selim.fuentesc@gmail.com>
2026-06-01 22:51:29 -0400

    restore working homepage

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-var express = require('express');
-var cors = require('cors');
-var multer = require('multer');
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static("public"));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+var upload = multer();
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/public/index.html");
 });
 
-var upload = multer({ dest: 'uploads/' });
-
-app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
-  console.log("FCC llegó al endpoint");
-  console.log(req.file);
-
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res) {
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
     size: req.file.size
   });
+});
+
+var listener = app.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on port " + listener.address().port);
 });
